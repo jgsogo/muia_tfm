d9a8e6070e6271ecefba792584e9399989e4e95e
jgsogo <jgsogo@gmail.com>
2015-03-30 14:24:08 +0200

    numeros de figuras

--- doc/conf.py
+++ doc/conf.py
@@ -18,7 +18,7 @@ import os
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
-#sys.path.insert(0, os.path.abspath('.'))
+sys.path.insert(0, os.path.abspath('extensions'))
 
 # -- General configuration ------------------------------------------------
 
@@ -34,6 +34,7 @@ extensions = [
     'sphinx.ext.mathjax',
     'sphinx.ext.graphviz',
     'sphinxcontrib.bibtex',
+    'numfig',
 ]
 
 # Add any paths that contain templates here, relative to this directory.
