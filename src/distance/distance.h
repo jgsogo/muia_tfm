96dc82b9c4fbd803e8174756b12421cd030ad427
Javier G. Sogo <jgsogo@gmail.com>
2015-05-22 00:10:49 +0200

    measuring distance between vectors of synsets

--- src/distance/distance.h
+++ src/distance/distance.h
@@ -10,9 +10,10 @@ namespace wn {
 	class WN_DISTANCE_EXPORT distance {
 		public:
 			distance(const wnb::wordnet& wordnet);
-			virtual float operator()(const wnb::synset& s1, const wnb::synset& s2) const = 0;
 			virtual float min() const; //! min distance by default is 0
 			virtual float max() const; //! max distance by default is std::numeric_limits<float>::max();
+			virtual float operator()(const wnb::synset& s1, const wnb::synset& s2) const = 0;
+			virtual std::pair<std::vector<std::pair<wnb::synset, wnb::synset>>, float> min_distance(std::vector<wnb::synset> v1, std::vector<wnb::synset> v2) const;
 		protected:
 			const wnb::wordnet& wordnet;
 	};
