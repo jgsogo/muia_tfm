a0fe45a5d1bfc0e797f8b8d49a38bc3fdb7ddd37
Javier G. Sogo <jgsogo@users.noreply.github.com>
2015-05-23 20:03:44 +0200

    compute hypernym path between elements and lowest_common hypernym of two synsets

--- src/wordnet/hyperonym_graph.h
+++ src/wordnet/hyperonym_graph.h
@@ -12,11 +12,15 @@ namespace wn {
 
 			static std::map<wnb::synset, std::size_t> hypernym_map(const wnb::wordnet& wordnet, const wnb::synset& s, bool instance_hyperonym = true);
             static std::vector<wnb::synset> orphans(const wnb::wordnet& wordnet, bool instance_hyperonym = true);
+            static std::vector<std::vector<wnb::synset>> hypernym_path(const wnb::wordnet& wordnet, const wnb::synset& from, const wnb::synset& to, bool instance_hyperonym = true);
+            static std::vector<wnb::synset> lowest_hypernym(const wnb::wordnet& wordnet, const wnb::synset& s1, const wnb::synset& s2, bool instance_hyperonym = true);
 
             std::map<wnb::synset, std::size_t> hypernym_map(const wnb::synset& s) const;
             std::vector<wnb::synset> orphans() const;
 			std::size_t max_depth() const;
 			std::size_t get_max_depth(const wnb::synset& s) const;
+			std::vector<std::vector<wnb::synset>> hypernym_path(const wnb::synset& from, const wnb::synset& to) const;
+			std::vector<wnb::synset> lowest_hypernym(const wnb::synset& s1, const wnb::synset& s2) const;
 		protected:
 			struct data;
 			data* d;
