b4387c524ddddcb6a3e17da7cfdfdf20dfa33875
Javier G. Sogo <jgsogo@users.noreply.github.com>
2015-05-25 17:48:07 +0200

    working with conceptual-graphs

---
+++ src/conceptual_graph/unl_graph.cpp
@@ -0,0 +1,48 @@
+
+#include "unl_graph.h"
+#include "conceptual_graph_data.h"
+
+using namespace wn;
+
+class unl_graph_printer : public boost::default_writer {
+    public:
+        unl_graph_printer(const _t_graph& g, const unl_graph& unl_data) : graph(g), unl_data(unl_data) {}
+        void operator()(std::ostream& os, const boost::graph_traits<_t_graph>::vertex_descriptor& v) const {
+            os << "[label=\"" << graph[v];
+            auto attributes = unl_data.get_attributes(v);
+            for (auto& attr : attributes) {
+                os << "@" << attr << ", ";
+            }
+            os << "\"]";
+        }
+        void operator()(std::ostream& os, const boost::graph_traits<_t_graph>::edge_descriptor& e) const {
+            const relation& rel = graph[e];
+            os << "[label=\"" << rel << "\"]";
+        }
+    private:
+        const _t_graph& graph;
+        const unl_graph& unl_data;
+};
+
+
+unl_graph::unl_graph() : conceptual_graph() {}
+
+unl_graph::~unl_graph() {}
+
+void unl_graph::add_attribute(const synset_id& id, const std::string& attr) {
+    attributes[id].push_back(attr);
+}
+
+std::vector<std::string> unl_graph::get_attributes(const synset_id& id) const {
+    std::vector<std::string> ret;
+    auto it_attr = attributes.find(id);
+    if (it_attr != attributes.end()) {
+        std::copy(it_attr->second.begin(), it_attr->second.end(), std::back_inserter(ret));
+    }
+    return ret;
+}
+
+void unl_graph::print(std::ostream& os) const {
+    unl_graph_printer writer(d->graph, *this);
+    d->print(os, d->graph, writer);
+}
---
+++ src/ex_conceptual_graph/main.cpp
@@ -0,0 +1,98 @@
+
+#include <iostream>
+#include <vector>
+
+#include <boost/filesystem.hpp>
+#include "wnb/core/wordnet.hh"
+#include "../conceptual_graph/conceptual_graph.h"
+#include "../conceptual_graph/unl_graph.h"
+
+using namespace std;
+using namespace wnb;
+namespace fs = ::boost::filesystem;
+
+
+int main(int argc, char** argv) {
+	if (argc != 3) {
+		cout << "usage: " << argv[0] << " <path/to/wordnet-3.0/> <path/to/semcor/>" << endl;
+		exit(1);
+	}
+    cout << endl;
+    cout << "# Loading WordNet" << endl;
+    cout << "#-------------------------------" << endl;
+	wordnet wn(argv[1], true);
+
+    /*
+    cout << endl;
+    cout << "# Reading SemCor" << endl;
+    cout << "#-------------------------------" << endl;
+	wn::semcor corpus(wn);
+    string basepath = argv[2];
+
+    // Parse files in SemCor corpus
+    auto read_corpus_dir = [&corpus](const string& directory) {
+        if(fs::is_directory(directory)) {
+            cout << "## Reading files in " << directory << endl;
+            fs::recursive_directory_iterator it(directory);
+            fs::recursive_directory_iterator endit;
+            while(it != endit) {
+                if(fs::is_regular_file(*it) && it->path().extension() == ".xml") {
+                    auto filename = fs::absolute(it->path());
+                    auto doc_index = corpus.add_document(filename.string());
+                    cout << "\t" << filename << " --> " << doc_index.size() << " synsets." << endl;
+                }
+                ++it;
+            }
+        }
+    };
+    read_corpus_dir(basepath + "brown1/");
+    read_corpus_dir(basepath + "brown2/");
+    read_corpus_dir(basepath + "brownv/");
+    
+
+    // Print some stats
+    cout << endl;
+    cout << "# Stats about SemCor" << endl;
+    cout << "#-------------------------------" << endl;
+    auto documents = corpus.get_documents();
+    auto index = corpus.get_index();
+    cout << " - documents parsed: " << documents.size() << endl;
+    cout << " - index size: " << index.size() << endl;
+    */
+    
+    cout << endl;
+    cout << "# Build conceptual graph" << endl;
+    cout << "#-------------------------------" << endl;
+    // Build a sample cgraph
+    wn::conceptual_graph cgraph;
+    auto dog = wn.get_synsets("dog", pos_t::N)[0];
+    std::cout << " - dog >> " << dog << endl;
+    auto drink = wn.get_synsets("drink", pos_t::V)[0];
+    std::cout << " - drink >> " << drink << endl;
+    auto water = wn.get_synsets("water", pos_t::N)[0];
+    std::cout << " - water >> " << water << endl;
+    
+    auto id_drink = cgraph.add_node(drink);
+    auto id_dog = cgraph.add_node(dog);
+    auto id_water = cgraph.add_node(water);
+    cgraph.add_relation(id_drink, id_dog, 10);
+    cgraph.add_relation(id_drink, id_water, 12);
+
+    cgraph.print(std::cout);
+    
+    
+    cout << endl;
+    cout << "# Build UNL graph" << endl;
+    cout << "#-------------------------------" << endl;
+    // Build a sample unl_graph
+    wn::unl_graph unlgraph;
+    auto id_drink_unl = unlgraph.add_node(drink);
+    unlgraph.add_attribute(id_drink_unl, "attr1");
+    auto id_dog_unl = unlgraph.add_node(dog);
+    auto id_water_unl = unlgraph.add_node(water);
+    unlgraph.add_relation(id_drink_unl, id_dog_unl, 10);
+    unlgraph.add_relation(id_drink_unl, id_water_unl, 12);
+
+    unlgraph.print(std::cout);
+    
+}
