60d65176c298e5dcb67f5d53a8bf861c369c3cb1
Javier Garc?a Sogo <javier_garcia@etsii.upm.es>
2015-06-15 17:45:43 +0200

    hay que refactorizar la b?squeda en el mcgregor

--- src/conceptual_graph/mcs_callback_store.h
+++ src/conceptual_graph/mcs_callback_store.h
@@ -82,7 +82,7 @@ namespace wn {
                     
                 }
 
-                std::cout << "---" << std::endl;
+                //std::cout << "---" << std::endl;
                 graphs.push_back(std::make_tuple(subgraph1, subgraph2, correspondence, similarity));
                 return (true);
             }
