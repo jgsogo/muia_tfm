4eb38b7bfd78a6477067e9bb2346ee9604298936
Javier G. Sogo <jgsogo@gmail.com>
2015-06-18 22:49:42 +0200

    rearranging dirs

--- data/samples/dot2png.py
+++ data/samples/dot2png.py
@@ -2,9 +2,21 @@
 import os
 from subprocess import check_call
 
-files = [f for f in os.listdir('.') if os.path.isfile(f)]
-for f in files:
-	name, extension = os.path.splitext(f)
-	if extension == '.dot':
-                print(f)
-                check_call(['dot','-Tpng', f,'-o', name+'.png'])
+# Work current dir
+def work_dir(directory):
+    directory = os.path.abspath(directory)
+    print("Work in directory %r" % directory)
+    files = [os.path.join(directory, f) for f in os.listdir(directory) if os.path.isfile(os.path.join(directory, f))]
+    n = len(files)
+    i = 1
+    for f in files:
+	    name, extension = os.path.splitext(f)
+	    if extension == '.dot':
+                    print("%d/%d %s" %(i, n, f))
+                    check_call(['dot','-Tpng', f,'-o', os.path.join(directory, name+'.png')])
+	    i+=1
+                
+# Work subdirs
+work_dir('.')
+_ = [work_dir(os.path.join('.', d)) for d in os.listdir('.') if os.path.isdir(d)]
+
--- graphs/work_sample.R
+++ graphs/work_sample.R
@@ -10,58 +10,79 @@ parse_sample <- function(file) {
     data <- data.frame(read.csv(file=file, header=TRUE, sep="\t"))
 }
 
-plot_synset_tol <- function(data, title, img_name) {
+plot_synset_tol <- function(data, title, save_path) {
     # Credit: http://www.cookbook-r.com/Graphs/Plotting_means_and_error_bars_(ggplot2)/   
-    d_sub <- subset(data, relation.tolerance==0)
-    d_sub <- d_sub[, c("translator", "synset.tolerance", "similarity.value")]
-    colnames(d_sub) <- c("Traductor", "tol", "sim")
-    d_sub$Traductor <- factor(d_sub$Traductor, levels=c("google", "yandex"), labels=c("Google", "Yandex"))
-    
-    tgc <- summarySE(d_sub, measurevar="sim", groupvars=c("Traductor", "tol"))
-
-    # Standard error of the mean
-    pd <- position_dodge(0.01) # move them .05 to the left and right
-    g <- ggplot(tgc, aes(x=tol, y=sim, colour=Traductor)) + 
-         geom_errorbar(aes(ymin=sim-se, ymax=sim+se), width=.1, position=pd) +
-         geom_line() +
-         geom_point() +
-#          ylim(0.0, 1.0) +
-         ylab("Similaridad") + xlab("Tolerancia entre conceptos\nTolerancia entre relaciones = 0.0") +
-         labs(title=title)
-    
-    ggsave(paste(img_name, "synset_tol.png", sep="-"), g)
-    g
+    tol_rel_values = unique(data[, c("relation.tolerance")])
+    for (tol_rel in tol_rel_values) {
+      d_sub <- subset(data, relation.tolerance==tol_rel)
+      d_sub <- d_sub[, c("translator", "synset.tolerance", "similarity.value")]
+      colnames(d_sub) <- c("Traductor", "tol", "sim")
+      d_sub$Traductor <- factor(d_sub$Traductor, levels=c("google", "yandex"), labels=c("Google", "Yandex"))
+      
+      tgc <- summarySE(d_sub, measurevar="sim", groupvars=c("Traductor", "tol"))
+  
+      # Standard error of the mean
+      pd <- position_dodge(0.01) # move them .05 to the left and right
+      g <- ggplot(tgc, aes(x=tol, y=sim, colour=Traductor)) + 
+           geom_errorbar(aes(ymin=sim-se, ymax=sim+se), width=.1, position=pd) +
+           geom_line() +
+           geom_point() +
+  #          ylim(0.0, 1.0) +
+           ylab("Similaridad") + xlab(paste("Tolerancia entre conceptos\nTolerancia entre relaciones = ", tol_rel, sep="")) +
+           labs(title=title)
+      
+      filename = paste("synset_tol", "relation_tol", tol_rel, sep="-")
+      filename = paste(filename, "png", sep=".")
+      filepath = file.path(save_path, filename)
+      print(filepath)
+      g
+      ggsave(filepath, g)
+    }
 }
 
-plot_relation_tol <- function(data, title, img_name) {
+plot_relation_tol <- function(data, title, save_path) {
     # Credit: http://www.cookbook-r.com/Graphs/Plotting_means_and_error_bars_(ggplot2)/   
-    d_sub <- subset(data, synset.tolerance==0)
-    d_sub <- d_sub[, c("translator", "relation.tolerance", "similarity.value")]
-    colnames(d_sub) <- c("Traductor", "tol", "sim")
-    d_sub$Traductor <- factor(d_sub$Traductor, levels=c("google", "yandex"), labels=c("Google", "Yandex"))
-    
-    tgc <- summarySE(d_sub, measurevar="sim", groupvars=c("Traductor", "tol"))
-    
-    # Standard error of the mean
-    pd <- position_dodge(0.01) # move them .05 to the left and right
-    g <- ggplot(tgc, aes(x=tol, y=sim, colour=Traductor)) + 
-        geom_errorbar(aes(ymin=sim-se, ymax=sim+se), width=.1, position=pd) +
-        geom_line() +
-        geom_point() +
-#         ylim(0.0, 1.0) +
-        ylab("Similaridad") + xlab("Tolerancia entre relaciones\nTolerancia entre conceptos = 0.0") +
-        labs(title=title)
-    
-    ggsave(paste(img_name, "relation_tol.png", sep="-"), g)
-    g
+    tol_syn_values = unique(data[, c("synset.tolerance")])
+    for (tol_syn in tol_syn_values) {
+      d_sub <- subset(data, synset.tolerance==tol_syn)
+      d_sub <- d_sub[, c("translator", "relation.tolerance", "similarity.value")]
+      colnames(d_sub) <- c("Traductor", "tol", "sim")
+      d_sub$Traductor <- factor(d_sub$Traductor, levels=c("google", "yandex"), labels=c("Google", "Yandex"))
+      
+      tgc <- summarySE(d_sub, measurevar="sim", groupvars=c("Traductor", "tol"))
+      
+      # Standard error of the mean
+      pd <- position_dodge(0.01) # move them .05 to the left and right
+      g <- ggplot(tgc, aes(x=tol, y=sim, colour=Traductor)) + 
+          geom_errorbar(aes(ymin=sim-se, ymax=sim+se), width=.1, position=pd) +
+          geom_line() +
+          geom_point() +
+  #         ylim(0.0, 1.0) +
+          ylab("Similaridad") + xlab(paste("Tolerancia entre relaciones\nTolerancia entre conceptos = ", tol_syn, sep="")) +
+          labs(title=title)
+      
+      filename = paste("relation_tol", "synset_tol", tol_syn, sep="-")
+      filename = paste(filename, "png", sep=".")
+      filepath = file.path(save_path, filename)
+      print(filepath)
+      g
+      ggsave(filepath, g)
+    }
 }
 
 
 work_sample <- function(file, title) {
     data <- parse_sample(file)
-    img_name = substr(file, 0, which(strsplit(file, '')[[1]]=='-')-1)
-    plot_synset_tol(data, title, img_name)    
-    plot_relation_tol(data, title, img_name)
+
+    filepath = dirname(file)
+    img_name = basename(file)
+    img_name = substr(img_name, 0, which(strsplit(img_name, '')[[1]]=='.')-1)
+    save_path = file.path(filepath, paste(img_name, "brief", sep="-"))
+    
+    dir.create(save_path, showWarnings = FALSE)
+    
+    plot_synset_tol(data, title, save_path)    
+    plot_relation_tol(data, title, save_path)
 
 }
 
