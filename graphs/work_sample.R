0b945cc520ff1df6f43f4eb21a4b51b8486d91a9
Javier Garc?a Sogo <javier_garcia@etsii.upm.es>
2015-06-18 18:20:22 +0200

    -los gr?ficos se guardan en im?genes

--- graphs/work_sample.R
+++ graphs/work_sample.R
@@ -3,33 +3,65 @@
 library(data.table)
 require(ggplot2)
 library(reshape)
+source("summarySE.R")
+
 
 parse_sample <- function(file) {
-    data <- data.table(read.csv(file=file, header=TRUE, sep="\t"))
+    data <- data.frame(read.csv(file=file, header=TRUE, sep="\t"))
 }
 
-plot_synset_tol <- function(data, title) {
-    google <- subset(data, data$translator=="google")
-    yandex <- subset(data, data$translator=="yandex")    
+plot_synset_tol <- function(data, title, img_name) {
+    # Credit: http://www.cookbook-r.com/Graphs/Plotting_means_and_error_bars_(ggplot2)/   
+    d_sub <- subset(data, relation.tolerance==0)
+    d_sub <- d_sub[, c("translator", "synset.tolerance", "similarity.value")]
+    colnames(d_sub) <- c("Traductor", "tol", "sim")
+    d_sub$Traductor <- factor(d_sub$Traductor, levels=c("google", "yandex"), labels=c("Google", "Yandex"))
     
-    x <- data[, synset.tolerance]
-    y1 <- google[, similarity.value]
-    y2 <- yandex[, similarity.value]
-    df <- data.frame(tol=x, google=y1, yandex=y2)
+    tgc <- summarySE(d_sub, measurevar="sim", groupvars=c("Traductor", "tol"))
+
+    # Standard error of the mean
+    pd <- position_dodge(0.01) # move them .05 to the left and right
+    g <- ggplot(tgc, aes(x=tol, y=sim, colour=Traductor)) + 
+         geom_errorbar(aes(ymin=sim-se, ymax=sim+se), width=.1, position=pd) +
+         geom_line() +
+         geom_point() +
+#          ylim(0.0, 1.0) +
+         ylab("Similaridad") + xlab("Tolerancia entre conceptos\nTolerancia entre relaciones = 0.0") +
+         labs(title=title)
     
-    g <- ggplot(df, aes(tol)) +
-        geom_line(aes(y=yandex, colour="Yandex")) +
-        geom_line(aes(y=google, colour="Google")) +
-        scale_colour_manual("Traductores", 
-                            breaks = c("Yandex", "Google"),
-                            values = c("blue", "red"))
-    g <- g + ylab("Similaridad") + xlab("Tolerancia entre conceptos")
-    g <- g + labs(title=title)
+    ggsave(paste(img_name, "synset_tol.png", sep="-"), g)
     g
 }
 
+plot_relation_tol <- function(data, title, img_name) {
+    # Credit: http://www.cookbook-r.com/Graphs/Plotting_means_and_error_bars_(ggplot2)/   
+    d_sub <- subset(data, synset.tolerance==0)
+    d_sub <- d_sub[, c("translator", "relation.tolerance", "similarity.value")]
+    colnames(d_sub) <- c("Traductor", "tol", "sim")
+    d_sub$Traductor <- factor(d_sub$Traductor, levels=c("google", "yandex"), labels=c("Google", "Yandex"))
+    
+    tgc <- summarySE(d_sub, measurevar="sim", groupvars=c("Traductor", "tol"))
+    
+    # Standard error of the mean
+    pd <- position_dodge(0.01) # move them .05 to the left and right
+    g <- ggplot(tgc, aes(x=tol, y=sim, colour=Traductor)) + 
+        geom_errorbar(aes(ymin=sim-se, ymax=sim+se), width=.1, position=pd) +
+        geom_line() +
+        geom_point() +
+#         ylim(0.0, 1.0) +
+        ylab("Similaridad") + xlab("Tolerancia entre relaciones\nTolerancia entre conceptos = 0.0") +
+        labs(title=title)
+    
+    ggsave(paste(title, "relation_tol.png", sep="-"), g)
+    g
+}
+
+
 work_sample <- function(file, title) {
     data <- parse_sample(file)
-    plot_synset_tol(data, title)
+    img_name = substr(file, 0, which(strsplit(file, '')[[1]]=='-')-1)
+    plot_synset_tol(data, title, img_name)    
+    plot_relation_tol(data, title, img_name)
+
 }
 
