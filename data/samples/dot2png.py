014c422ddad65d8d1f6b2d1c57148e41407d4cf1
Javier Garc?a Sogo <javier_garcia@etsii.upm.es>
2015-06-19 13:46:09 +0200

    option to clean all generated images from directory

--- data/samples/dot2png.py
+++ data/samples/dot2png.py
@@ -19,11 +19,28 @@ def work_dir(directory, force):
                 check_call(['dot','-Tpng', f,'-o', output])
         i+=1
 
-# Work subdirs
+# Clean directory
+def clean_dir(directory):
+    directory = os.path.abspath(directory)
+    print("Work in directory %r" % directory)
+    files = [os.path.join(directory, f) for f in os.listdir(directory) if os.path.isfile(os.path.join(directory, f))]
+    for f in files:
+        name, extension = os.path.splitext(f)
+        if extension == '.png':
+            os.remove(f)
+    if directory.endswith("-brief"):
+        os.rmdir(directory)
 
 
 if __name__=='__main__':
     force = sys.argv[1]=='force' if len(sys.argv)==2 else False
+    clean = sys.argv[1]=='clean' if len(sys.argv)==2 else False
     
-    work_dir('.', force)
-    _ = [work_dir(os.path.join('.', d), force) for d in os.listdir('.') if os.path.isdir(d)]
+    if clean:
+        clean_dir('.')
+        _ = [clean_dir(os.path.join('.', d)) for d in os.listdir('.') if os.path.isdir(d)]
+                
+    else:
+        work_dir('.', force)
+        _ = [work_dir(os.path.join('.', d), force) for d in os.listdir('.') if os.path.isdir(d)]
+     
