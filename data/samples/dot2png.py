82a378e6689c5c1827eab23c28650e1f995d91cf
Javier G. Sogo <jgsogo@users.noreply.github.com>
2015-06-19 10:41:28 +0200

    no se crean las im?genes de los grafos si ya est?n creadas (salvo que se indique con un 'force')

--- data/samples/dot2png.py
+++ data/samples/dot2png.py
@@ -1,22 +1,29 @@
 
 import os
+import sys
 from subprocess import check_call
 
 # Work current dir
-def work_dir(directory):
+def work_dir(directory, force):
     directory = os.path.abspath(directory)
     print("Work in directory %r" % directory)
     files = [os.path.join(directory, f) for f in os.listdir(directory) if os.path.isfile(os.path.join(directory, f))]
     n = len(files)
     i = 1
     for f in files:
-	    name, extension = os.path.splitext(f)
-	    if extension == '.dot':
-                    print("%d/%d %s" %(i, n, f))
-                    check_call(['dot','-Tpng', f,'-o', os.path.join(directory, name+'.png')])
-	    i+=1
-                
-# Work subdirs
-work_dir('.')
-_ = [work_dir(os.path.join('.', d)) for d in os.listdir('.') if os.path.isdir(d)]
+        name, extension = os.path.splitext(f)
+        if extension == '.dot':
+            print("%d/%d %s" %(i, n, f))
+            output = os.path.join(directory, name+'.png')
+            if force or not os.path.isfile(output):
+                check_call(['dot','-Tpng', f,'-o', output])
+        i+=1
 
+# Work subdirs
+
+
+if __name__=='__main__':
+    force = sys.argv[1]=='force' if len(sys.argv)==2 else False
+    
+    work_dir('.', force)
+    _ = [work_dir(os.path.join('.', d), force) for d in os.listdir('.') if os.path.isdir(d)]
