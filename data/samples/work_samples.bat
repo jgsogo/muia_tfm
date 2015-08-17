00214065f79eae7258862c1a7d4ecf83563957cb
Javier Garc?a Sogo <javier_garcia@etsii.upm.es>
2015-08-17 10:06:31 +0200

    preparing to store

---
+++ data/samples/work_samples.bat
@@ -0,0 +1,30 @@
+
+TITLE work_sample sample #01
+"../../install/bin/work_sample.exe" ../../install/data/wordnet3.1/dict/ ../../install/data/semcor/ sample01.txt
+
+TITLE work_sample sample #02
+::"../../install/bin/work_sample.exe" ../../install/data/wordnet3.1/dict/ ../../install/data/semcor/ sample02.txt
+
+TITLE work_sample sample #03
+"../../install/bin/work_sample.exe" ../../install/data/wordnet3.1/dict/ ../../install/data/semcor/ sample03.txt
+
+TITLE work_sample sample #04
+"../../install/bin/work_sample.exe" ../../install/data/wordnet3.1/dict/ ../../install/data/semcor/ sample04.txt
+
+TITLE work_sample sample #05
+"../../install/bin/work_sample.exe" ../../install/data/wordnet3.1/dict/ ../../install/data/semcor/ sample05.txt
+
+TITLE work_sample sample #06
+::"../../install/bin/work_sample.exe" ../../install/data/wordnet3.1/dict/ ../../install/data/semcor/ sample06.txt
+
+TITLE work_sample sample #07
+"../../install/bin/work_sample.exe" ../../install/data/wordnet3.1/dict/ ../../install/data/semcor/ sample07.txt
+
+TITLE work_sample sample #08
+"../../install/bin/work_sample.exe" ../../install/data/wordnet3.1/dict/ ../../install/data/semcor/ sample08.txt
+
+TITLE work_sample sample #09
+"../../install/bin/work_sample.exe" ../../install/data/wordnet3.1/dict/ ../../install/data/semcor/ sample09.txt
+
+TITLE work_sample sample #10
+"../../install/bin/work_sample.exe" ../../install/data/wordnet3.1/dict/ ../../install/data/semcor/ sample10.txt
---
+++ data/samples/working.bat
@@ -0,0 +1,16 @@
+
+TITLE work_sample sample #07
+::ping 192.0.2.2 -n 1 -w 10000 > nul
+::"../../install/bin/work_sample.exe" ../../install/data/wordnet3.1/dict/ ../../install/data/semcor/ sample07.txt
+
+TITLE work_sample sample #09
+::ping 192.0.2.2 -n 1 -w 10000 > nul
+"../../install/bin/work_sample.exe" ../../install/data/wordnet3.1/dict/ ../../install/data/semcor/ sample09.txt
+
+TITLE work_sample sample #06
+::ping 192.0.2.2 -n 1 -w 10000 > nul
+::"../../install/bin/work_sample.exe" ../../install/data/wordnet3.1/dict/ ../../install/data/semcor/ sample06.txt
+
+TITLE work_sample sample #02
+ping 192.0.2.2 -n 1 -w 10000 > nul
+"../../install/bin/work_sample.exe" ../../install/data/wordnet3.1/dict/ ../../install/data/semcor/ sample02.txt
